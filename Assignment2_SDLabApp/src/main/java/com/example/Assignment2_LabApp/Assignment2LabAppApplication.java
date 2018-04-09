61d540e4fe75231b8d3feda95adef42ca3580bf9
Anda Tatomir <andatatomir@bitbucket.org>
2018-04-10 01:16:01 +0300

    Updated repo structure

---
+++ Assignment2_SDLabApp/src/main/java/com/example/Assignment2_LabApp/Assignment2LabAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.Assignment2_LabApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment2LabAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment2LabAppApplication.class, args);
+	}
+}
---
+++ Assignment2_SDLabApp/src/test/java/com/example/Assignment2_LabApp/Assignment2LabAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.Assignment2_LabApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Assignment2LabAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
